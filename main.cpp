a0e692f85d028f99eb36e1fc5096418098729c56
huiminlim <e0036379@u.nus.edu>
2023-04-07 17:08:29 +0800

    Add Poco types and byte order

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
+#include <Poco/Foundation.h>
+#include <Poco/ByteOrder.h>
+
 int main(int argc, char *argv[])
 {
     std::cout << "Hello, world!" << std::endl;
+    
+    Poco::UInt16 port = 80;
+    Poco::UInt16 networkPort = Poco::ByteOrder::toNetwork(port);
+    std::cout << port << " " << networkPort << std::endl;
+
     return EXIT_SUCCESS;
 }
